c7d6d609c7b142700432c1c60f912193d40d7760
baymerakli <ozgurtas@gmail.com>
2016-01-26 00:25:29 +0200

    mouse inputs

--- core/src/com/mygdx/game/GraphicsDemo.java
+++ core/src/com/mygdx/game/GraphicsDemo.java
@@ -2,55 +2,51 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
 
 public class GraphicsDemo implements ApplicationListener {
     private SpriteBatch batch;
-    private TextureAtlas textureAtlas;
+    private Texture texture;
     private Sprite sprite;
-    private int currentFrame = 1;
-    private String currentAtlasKey = new String("0001");
 
     @Override
     public void create() {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
         batch = new SpriteBatch();
-        textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
-        AtlasRegion region = textureAtlas.findRegion("0001");
-        sprite = new Sprite(region);
-        sprite.setPosition(120, 100);
-        sprite.scale(2.5f);
-        Timer.schedule(new Task(){
-                @Override
-                public void run() {
-                    currentFrame++;
-                    if(currentFrame > 20)
-                        currentFrame = 1;
 
-                    // ATTENTION! String.format() doesnt work under GWT for god knows why...
-                    currentAtlasKey = String.format("%04d", currentFrame);
-                    sprite.setRegion(textureAtlas.findRegion(currentAtlasKey));
-                }
-            }
-            ,0,1/30.0f);
+        texture = new Texture(Gdx.files.internal("data/0001.png"));
+        sprite = new Sprite(texture);
+        sprite.setPosition(w/2 -sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        textureAtlas.dispose();
+        texture.dispose();
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite.translateX(-1f);
+            else
+                sprite.translateX(-10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite.translateX(1f);
+            else
+                sprite.translateX(10.0f);
+        }
         batch.begin();
         sprite.draw(batch);
         batch.end();
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,18 +3,16 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.GraphicsDemo;
-import com.mygdx.game.GraphicsDemo1;
-import com.mygdx.game.GraphicsDemo2;
-import com.mygdx.game.GraphicsDemo3;
-import com.mygdx.game.MyGdxGame;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 //		new LwjglApplication(new MyGdxGame(), config);
-//		new LwjglApplication(new GraphicsDemo(), config);
+		new LwjglApplication(new GraphicsDemo(), config);
 //		new LwjglApplication(new GraphicsDemo1(), config);
 //		new LwjglApplication(new GraphicsDemo2(), config);
-		new LwjglApplication(new GraphicsDemo3(), config);
+//		new LwjglApplication(new GraphicsDemo3(), config);
+//		new LwjglApplication(new GraphicsDemo4(), config);
 	}
 }
